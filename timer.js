9ca19055cd494a6b7017a92ef25b625645e61df8
Jake Chung <jakechung2222@gmail.com>
2020-11-18 20:50:42 -0500

    We finished our overall-functional timer today.

--- timer.js
+++ timer.js
@@ -2,34 +2,43 @@ var but5min = document.getElementById("5min");
 var but10min = document.getElementById("10min");
 var but15min = document.getElementById("15min");
 var butCust = document.getElementById("custom-run");
+var butReset = document.getElementById("reset");
 
 var minute = 00;
 var second = 00;
 var display = document.getElementById("remaining");
 
+var globalTimer = null;
+var tickSound = new Audio('clock-tick.mp3');
+var alarmSound = new Audio('Alarmclockringing.mp3');
+
+function updateDisplay() {
+
+    // TODO: show two digit formatting.
+    //    Today, we show '5:0'.  This should be '5:00'.
+    //
+    display.innerHTML =  minute.toString() + ":" + second.toString();
+    tickSound.play();
+    --second;
+    if (second <= 0) {
+        --minute;
+        second=59;
+    }
+
+    if (minute < 0) {
+        clearInterval(globalTimer);
+        alarmSound.play();
+    }
+}
 
 function setTimer(m,s) {
+    clearInterval(globalTimer);
     minute = m;
     second = s;
 
     total_sec = minute * 60 + second;
 
-    // TODO: show two digit formatting.
-    //    Today, we show '5:0'.  This should be '5:00'.
-    //
-
-    display.innerHTML =  minute.toString() + ":" + second.toString();
-    while (total_sec > 0) {
-        --total_sec;    // total_sec = total_sec - 1;   (++total_sec) or (total_sec++)
-
-        // TODO: 
-        //    Count down by 1
-        minute = total_sec / 60;
-        second = total_sec % 60;
-        display.innerHTML =  minute.toString() + ":" + second.toString();
-        
-        sleep(1000);
-    }
+    globalTimer = setInterval(updateDisplay, 1000);
 }
 
 but5min.addEventListener('click',  function() { setTimer(5,0); }  );
@@ -43,5 +52,18 @@ butCust.addEventListener('click',  function() {
     //    We only want integer.  If it isn't number, send alert.
     //
     setTimer( parseInt(user_min), parseInt(user_sec) );
+});
+butReset.addEventListener('click', function(){
+    clearInterval(globalTimer);
+    globalTimer = null;
+    minute = 00;
+    second = 00;
+    document.getElementById("min").value = "00";
+    document.getElementById("sec").value = "00";
+
+    tickSound.pause();
+    tickSound.currentTime = 0;
+    alarmSound.pause();
+    alarmSound.currentTime = 0;
 });
 
